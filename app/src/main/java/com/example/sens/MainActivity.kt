900b8bb5ac4f2bebc242159404039f3d2eca172a
Mehmet Küçükdemir <mehmetmac1@gmail.com>
2023-09-07 19:13:03 +0300

    accelerometer, gyroscope and gps are written in different files

--- app/src/main/java/com/example/sens/MainActivity.kt
+++ app/src/main/java/com/example/sens/MainActivity.kt
@@ -3,10 +3,18 @@ package com.example.sens
 import android.content.Context
 import android.content.pm.PackageManager
 import android.hardware.Sensor
+import android.hardware.Sensor.TYPE_ACCELEROMETER
+import android.hardware.Sensor.TYPE_ALL
+import android.hardware.Sensor.TYPE_GYROSCOPE
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.os.Environment
+import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -34,16 +42,22 @@ import java.io.FileWriter
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.Date
+import java.util.concurrent.locks.ReentrantLock
 
 class MainActivity : ComponentActivity() {
     private val SENSOR_PERMISSION_REQUEST_CODE = 1
-    private lateinit var sensorThread: Thread
+    private val STORAGE_PERMISSION_REQUEST_CODE = 123
     private lateinit var sensorManager: SensorManager
-    private lateinit var accelerometerSensor: Sensor
-    private lateinit var sensorEventListener: SensorEventListener
     private val viewModel: SensorDataViewModel by viewModels()
     private val sdf = SimpleDateFormat("dd/MM/yyyy HH:mm:ss.SSS")
-    private var now: Date? = null
+
+    private lateinit var accelerometerSensor: Sensor
+    private lateinit var gyroscopeSensor: Sensor
+    private lateinit var locationManager: LocationManager
+
+    private lateinit var accelerometer_writer:FileWriter
+    private lateinit var gyroscope_writer:FileWriter
+    private lateinit var gps_writer:FileWriter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,6 +74,10 @@ class MainActivity : ComponentActivity() {
             // Display the sensor data Composable
             DisplaySensorData(viewModel.sensorData)
         }
+        // Files to write data
+        accelerometer_writer =  FileWriter(File(Environment.getExternalStorageDirectory(), "accelerometer_data.txt"), true)
+        gyroscope_writer =  FileWriter(File(Environment.getExternalStorageDirectory(), "gyroscope_data.txt"), true)
+        gps_writer = FileWriter(File(Environment.getExternalStorageDirectory(), "gps_data.txt"), true)
 
         // Request sensor permission when the activity is created
         requestSensorPermission()
@@ -68,31 +86,53 @@ class MainActivity : ComponentActivity() {
         startSensorDataCollection()
     }
 
-    private fun requestSensorPermission() {
-        if (ContextCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
-                SENSOR_PERMISSION_REQUEST_CODE
-            )
-        }
+    override fun onDestroy() {
+        super.onDestroy()
+        accelerometer_writer.close()
+        gyroscope_writer.close()
+        gps_writer.close()
     }
 
-    private fun startSensorDataCollection() {
-        sensorThread = Thread {
-            sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-            accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+    private fun requestSensorPermission() {
+        val locationPermission = android.Manifest.permission.ACCESS_FINE_LOCATION
+        val sensorPermission = android.Manifest.permission.BODY_SENSORS
 
-            sensorEventListener = object : SensorEventListener {
+        val permissionsToRequest = mutableListOf<String>()
+
+        if (ContextCompat.checkSelfPermission(this, locationPermission) != PackageManager.PERMISSION_GRANTED) {
+            permissionsToRequest.add(locationPermission)
+        }
+
+        if (ContextCompat.checkSelfPermission(this, sensorPermission) != PackageManager.PERMISSION_GRANTED) {
+            permissionsToRequest.add(sensorPermission)
+        }
+
+        if (permissionsToRequest.isNotEmpty()) {
+            ActivityCompat.requestPermissions(this, permissionsToRequest.toTypedArray(), SENSOR_PERMISSION_REQUEST_CODE)
+        }
+
+        val permissions = arrayOf(
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            android.Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+
+        ActivityCompat.requestPermissions(this, permissions, STORAGE_PERMISSION_REQUEST_CODE)
+    }
+
+
+    private fun startSensorDataCollection() {
+        val sensorThread = Thread {
+            sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+            locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+            accelerometerSensor = sensorManager.getDefaultSensor(TYPE_ACCELEROMETER)
+            gyroscopeSensor = sensorManager.getDefaultSensor(TYPE_GYROSCOPE)
+
+            val accelerometerListener = object : SensorEventListener {
                 override fun onSensorChanged(event: SensorEvent) {
-                    // Handle sensor data here
                     val sensorValues = event.values.joinToString(", ")
-                    viewModel.sensorData = "Sensor Data: $sensorValues"  // Update the greeting text
-                    writeToSensorDataFile(event.values)  // Write data to the file
+                    val sensorData = "$sensorValues"
+                    writeDataToFile(sensorData, accelerometer_writer)
                 }
 
                 override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
@@ -100,28 +140,70 @@ class MainActivity : ComponentActivity() {
                 }
             }
 
+            val gyroscopeListener = object : SensorEventListener {
+                override fun onSensorChanged(event: SensorEvent) {
+                    val sensorValues = event.values.joinToString(", ")
+                    val sensorData = "$sensorValues"
+                    writeDataToFile(sensorData, gyroscope_writer)
+                }
+
+                override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+                    // Handle accuracy changes if needed
+                }
+            }
+
+            val locationListener = object : LocationListener {
+                override fun onLocationChanged(location: Location) {
+                    val locationData = "${location.latitude}, ${location.longitude}"
+                    writeDataToFile(locationData, gps_writer)
+                }
+
+                override fun onProviderEnabled(provider: String) {}
+                override fun onProviderDisabled(provider: String) {}
+                override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
+            }
+
             sensorManager.registerListener(
-                sensorEventListener,
+                accelerometerListener,
                 accelerometerSensor,
                 SensorManager.SENSOR_DELAY_NORMAL
             )
+
+            sensorManager.registerListener(
+                gyroscopeListener,
+                gyroscopeSensor,
+                SensorManager.SENSOR_DELAY_NORMAL
+            )
+
+            // Request GPS updates
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                locationManager.requestLocationUpdates(
+                    //LocationManager.GPS_PROVIDER,
+                    //LocationManager.PASSIVE_PROVIDER,
+                    LocationManager.NETWORK_PROVIDER,
+                    0,
+                    0f,
+                    locationListener,
+                    Looper.getMainLooper()
+                )
+            }
         }
 
         sensorThread.start()
     }
 
-
-    private fun writeToSensorDataFile(sensorData: FloatArray) {
+    private fun writeDataToFile(data: String, writer: FileWriter) {
+        //println(data)
         try {
-            val file = File(getExternalFilesDir(null), "sensor_data.txt")
-            val writer = FileWriter(file, true) // Append data to the file
-
-            now = Date()
-            writer.write("[" + sdf.format(now) + "] " + sensorData.joinToString(", ") + "\n")
-
-            writer.close()
+            writer.write(sdf.format(Date()) + " " + data + "\n")
         } catch (e: IOException) {
             e.printStackTrace()
+        } finally {
+
         }
     }
 }
@@ -142,7 +224,7 @@ fun DisplaySensorData(sensorData: String) {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Sensor data is being written on your local drive",
         modifier = modifier
     )
 }
